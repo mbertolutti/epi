591cd7b3ffd48f154577b6b5aa01ad7c89fa1243
Mario Bertolutti <mario.bertolutti@gmail.com>
2021-10-08 17:55:49 +0000

    add a4_3.cpp

---
+++ a4_3.cpp
@@ -0,0 +1,29 @@
+// 4.3
+
+#include <fmt/core.h>
+
+void swap_bits(unsigned long long& bit_array, int x, int y)
+{
+    if (((bit_array >> x) & 1) != ((bit_array >> y) & 1))
+    {
+        long long bit_mask = (1LL << x) | (1LL << y);
+        bit_array ^= bit_mask;
+    }
+}
+
+void reverse_64_bit_word(unsigned long long& bit_array)
+{
+    for (int i = 0, j = 63; i < 31; ++i, --j)
+    {
+        swap_bits(bit_array, i, j);
+    }
+}
+
+int main()
+{
+    unsigned long long bit_array = 71LL;
+
+    fmt::print(fmt::format("bit_array: {0:b}\n", bit_array));
+    reverse_64_bit_word(bit_array);
+    fmt::print(fmt::format(" reversed: {0:b}\n\n", bit_array));
+}
